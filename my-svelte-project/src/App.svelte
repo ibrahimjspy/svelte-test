ae4cc69ea25380e0429ae251170686c86e43e6f1
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-07-03 17:08:15 +0500

    api and data loop integration , next up multi components test and glohal state test

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,15 +1,57 @@
 <script>
+	import axios from 'axios';
+	import {onMount} from 'svelte';
+
+	const requestAPI = async () => {
+		const data = {};
+		const call = await axios.get('https://catfact.ninja/fact').then(
+			res => console.log(res.data)
+		)
+	};
+
 	let firtname = "World";
 	let second = " King";
+	let count = 0;
+	let cats = [
+		{  name: 'Keyboard Cat' },
+		{  name: 'Maru' },
+		{  name: 'Henri The Existential Cat' }
+	];
+	const increment = () => {
+		console.log(count)
+		count = count + 1
+	}
 	$: name = firtname + "" + second
 </script>
 
 <main>
-	<h1>Hello {name}! Ibrahim was here</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {name}! Ibrahim was here with {count} times</h1>
+	<button on:click={increment}>State increment</button>
+	<button on:click={requestAPI}>APi call</button>
+
+	{#if count == 0}
+		<p>First time</p>
+	{/if}
+	{#if count !== 0}
+		<p>Not first time</p>
+	{/if}
+	{#each cats as cat}
+		<p>{cat.name}</p>
+	{/each}
 </main>
 
 <style>
+	button{
+		border-radius: 4px;
+		box-shadow: inset;
+		color: aqua;
+		background-color:blueviolet ;
+	}
+	button:hover{
+		color: black;
+		background-color: white;
+		transform: translateX(5px);
+	}
 	main {
 		text-align: center;
 		padding: 1em;
